e0e306e43c91e5a3f61222456968964b3e820f15
Horstexplorer <21320859+Horstexplorer@users.noreply.github.com>
2025-10-05 00:25:20 +0200

    updated dependencies

--- src/api/misc/data-generator.ts
+++ src/api/misc/data-generator.ts
@@ -1,7 +1,7 @@
-export function generateRandomData(length: number): Uint8Array {
+export function generateRandomData(length: number): Blob {
     const buffer = new Uint8Array(length)
     for (let i = 0; i < length; i += 65536) {
         crypto.getRandomValues(buffer.subarray(i, i + Math.min(length - i, 65536)))
     }
-    return buffer
+    return new Blob([buffer])
 }
